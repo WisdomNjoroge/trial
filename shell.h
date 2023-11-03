2205699350b196ed8dd69d6f463eb8a61a6419b1
marky376 <mmutuamark@gmail.com>
2023-11-03 05:46:09 +0300

    A commit on the main simple shell project.

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void display_prompt(void);
+
+
+#endif
